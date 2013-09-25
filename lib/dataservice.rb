3600d740f81a8f7b12331057f163d9c6daafd2b4
Samuel Scully <sbscully@gmail.com>
2013-09-25 14:50:30 +0100

    include upload module differently

--- lib/dataservice.rb
+++ lib/dataservice.rb
@@ -1,2 +1,6 @@
 require_relative 'dataservice/upload'
 require_relative 'dataservice/unique_upload_filename'
+
+module DataService
+  include Upload
+end
--- lib/dataservice/upload.rb
+++ lib/dataservice/upload.rb
@@ -1,40 +1,38 @@
 require 'carrierwave/mount'
 
-module DataService
-  module Upload
-    include CarrierWave::Mount
+module Upload
+  include CarrierWave::Mount
 
-    def mount_uploader(column, uploader=nil, options={}, &block)
-      super
+  def mount_uploader(column, uploader=nil, options={}, &block)
+    super
 
-      before_save :"write_#{column}_identifier"
-      after_save  :"store_#{column}!"
+    before_save :"write_#{column}_identifier"
+    after_save  :"store_#{column}!"
 
-      define_method(:read_uploader) do |column|
-        attributes[column]
-      end
-
-      define_method(:write_uploader) do |column, identifier|
-        attributes[column] = identifier
-      end
-
-      define_method(:"extract_#{column}_attribute!") do
-        upload = attributes.delete(column)
-        send("#{column}=", upload)
-      end
-
-      define_method(:load) do |attrs|
-        super(attrs)
-        if attributes[column].is_a? ActionDispatch::Http::UploadedFile
-          send(:"extract_#{column}_attribute!")
-        end
-        if attributes["remote_#{column}_url"]
-          url = attributes.delete("remote_#{column}_url")
-          send("remote_#{column}_url=", url)
-        end
-        self
-      end
-
+    define_method(:read_uploader) do |column|
+      attributes[column]
     end
+
+    define_method(:write_uploader) do |column, identifier|
+      attributes[column] = identifier
+    end
+
+    define_method(:"extract_#{column}_attribute!") do
+      upload = attributes.delete(column)
+      send("#{column}=", upload)
+    end
+
+    define_method(:load) do |attrs|
+      super(attrs)
+      if attributes[column].is_a? ActionDispatch::Http::UploadedFile
+        send(:"extract_#{column}_attribute!")
+      end
+      if attributes["remote_#{column}_url"]
+        url = attributes.delete("remote_#{column}_url")
+        send("remote_#{column}_url=", url)
+      end
+      self
+    end
+
   end
 end

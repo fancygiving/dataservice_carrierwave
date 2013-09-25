85045d7efd2e6f888dc050c87b69d1762d1d2e0d
Samuel Scully <sbscully@gmail.com>
2013-09-25 14:35:35 +0100

    clean up tests

--- spec/dataservice/unique_upload_filename_spec.rb
+++ spec/dataservice/unique_upload_filename_spec.rb
@@ -8,26 +8,26 @@ describe UniqueUploadFilename do
     model:              model,
     file:               file,
     mounted_as:         'photo'}) }
+  let(:filename) { UniqueUploadFilename.filename(uploader) }
 
   before do
     SecureRandom.stub(:uuid).and_return('18f0b691-928b-480f-ac04-05687c8f4bd3')
   end
 
   it 'makes a filename' do
-    expect(UniqueUploadFilename.filename(uploader))
-      .to eq('18f0b691-928b-480f-ac04-05687c8f4bd3-1380111980.jpg')
+    expect(filename).to eq('18f0b691-928b-480f-ac04-05687c8f4bd3-1380111980.jpg')
   end
 
-  it 'reuses the existing file name' do
-    model.stub(attributes: {'photo' => '/public/AN-EXISTING-FILE-PATH-380435980.jpg'})
-    expect(UniqueUploadFilename.filename(uploader))
-      .to eq('AN-EXISTING-FILE-PATH-1380111980.jpg')
-  end
+  describe 'with an existing filename' do
+    it 'reuses the existing file name' do
+      model.stub(attributes: {'photo' => '/public/AN-EXISTING-FILE-PATH-380435980.jpg'})
+      expect(filename).to eq('AN-EXISTING-FILE-PATH-1380111980.jpg')
+    end
 
-  it 'can handle existing filenames without extensions' do
-    model.stub(attributes: {'photo' => '/public/AN-EXISTING-FILE-PATH-380435980.'})
-    file.stub(extension: '')
-    expect(UniqueUploadFilename.filename(uploader))
-      .to eq('AN-EXISTING-FILE-PATH-1380111980.')
+    it 'without extension' do
+      model.stub(attributes: {'photo' => '/public/AN-EXISTING-FILE-PATH-380435980.'})
+      file.stub(extension: '')
+      expect(filename).to eq('AN-EXISTING-FILE-PATH-1380111980.')
+    end
   end
 end

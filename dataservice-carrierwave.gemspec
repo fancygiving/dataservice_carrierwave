b3393c0356a856ee531bcaba022b4ef133e1ebcf
Samuel Scully <sbscully@gmail.com>
2013-09-25 15:31:06 +0100

    fix loadpath issues

--- dataservice-carrierwave.gemspec
+++ dataservice-carrierwave.gemspec
@@ -1,13 +1,13 @@
 Gem::Specification.new do |s|
   s.name          = 'dataservice-carrierwave'
-  s.version       = '0.0.5'
+  s.version       = '0.0.6'
   s.date          = '2013-09-25'
   s.summary       = "Utility classes to allow carrierwave to be used with dataservice remote client."
   s.description   = "Utility classes to allow carrierwave to be used with dataservice remote client."
   s.authors       = ["Samuel Scully"]
   s.email         = 'dev@fancygiving.com'
-  s.files         = Dir["{lib}/**/*.rb", "bin/*", "LICENSE", "*.md"]
+  s.files         = Dir["lib/dataservice_carrierwave.rb", "lib/dataservice_carrierwave/unique_upload_filename.rb" "LICENSE", "*.md"]
   s.require_path  = 'lib'
-  s.homepage      = 'https://github.com/fancygiving/dataservice-client'
+  s.homepage      = 'https://github.com/fancygiving/dataservice-carrierwave'
   s.add_dependency('carrierwave')
 end
--- lib/dataservice_carrierwave.rb
+++ lib/dataservice_carrierwave.rb
@@ -1,2 +1,40 @@
-require_relative 'dataservice_carrierwave/upload'
+require 'carrierwave/mount'
 require_relative 'dataservice_carrierwave/unique_upload_filename'
+
+module DataService
+  module Upload
+    include CarrierWave::Mount
+
+    def mount_uploader(column, uploader=nil, options={}, &block)
+      super
+
+      before_save :"write_#{column}_identifier"
+      after_save  :"store_#{column}!"
+
+      define_method(:read_uploader) do |column|
+        attributes[column]
+      end
+
+      define_method(:write_uploader) do |column, identifier|
+        attributes[column] = identifier
+      end
+
+      define_method(:"extract_#{column}_attribute!") do
+        upload = attributes.delete(column)
+        send("#{column}=", upload)
+      end
+
+      define_method(:load) do |attrs|
+        super(attrs)
+        if attributes[column].is_a? ActionDispatch::Http::UploadedFile
+          send(:"extract_#{column}_attribute!")
+        end
+        if attributes["remote_#{column}_url"]
+          url = attributes.delete("remote_#{column}_url")
+          send("remote_#{column}_url=", url)
+        end
+        self
+      end
+    end
+  end
+end
